b2f094e0852a362004f85add00ffaf704549d2d5
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-04 23:55:54 +0530

    2nd episode

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-const heading = React.createElement("h1", 
-    {id:"heading"}, 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+const heading = React.createElement("h1",
+    { id: "heading" },
     "Hello World form React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(heading); 
 
 // const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, React.createElement("h1", {}, "Heading from a nested element!")));
 
-const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, [React.createElement("h1", {}, "H1"), React.createElement("h2", {}, "H2")]));
+const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "H11"), React.createElement("h2", {}, "H2")]));
 root.render(parent);
