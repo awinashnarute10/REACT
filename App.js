39f8d7a379ca5a6bfd9245e48d4ec20f4a5987dc
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-05 15:38:36 +0530

    ep 3.2

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React.createElement ==> is a function that returns object
 
+const heading = React.createElement("h1", {id:"heading"}, "This is react!!");
 
-const heading = React.createElement("h1",
-    { id: "heading" },
-    "Hello World form React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); 
-
-// const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, React.createElement("h1", {}, "Heading from a nested element!")));
-
-const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "H11"), React.createElement("h2", {}, "H2")]));
-root.render(parent);
+root.render(heading); //replace everything inside the root  in  html
