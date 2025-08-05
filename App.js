68f99b95b15fb257a190f19b96d9843188b3bf1a
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-06 03:59:56 +0530

    ep 3 concludes

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
  
-//React Element
-const heading = (<h1 id="heading" className="head" tabIndex="1">React Element using JSX</h1>);
+const elem = <span> React Element</span>
 
-//React Components:- Returns JSX elements and name starts with capital
+const heading = (<h1 id="heading" className="head" tabIndex="1">React Element using JSX {elem}</h1>);
+
+//React Components:- Returns JSX (or react element) and name starts with capital
 //1. Class Bases Component - OLD - Based on JS classes
 
 
@@ -13,9 +14,14 @@ const HeadingComponent = () => {
 return <div id="container">
     <h1 className="heading">React Functional Component</h1>
     {heading} 
+    {100+200}
     <HeadingComponent2/>
+    <HeadingComponent2></HeadingComponent2>
+    {HeadingComponent2()}
     </div>;
-    // above are the ways you render a element{} and a component</> inside another component aka component composition(only for the component part)
+    // above are the ways you render a element or a variable or a piece of js code{} and a component<></> or </> inside another component aka component composition(only for the component part)
+
+    //another way of rendering the compont inside component is { ()} as at the end of the way any js code can be put inside curly and component is nothing but JS function
 };
 const HeadingComponent2 = () => <h1>React Functional Component2</h1>;
 
