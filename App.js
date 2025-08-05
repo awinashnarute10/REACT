fad59ea4e61c75625f5aa6245cea491c6bb49b5f
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-06 03:33:42 +0530

    ep 3.4

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
  
-//JSX - (JS XML) - HTML like or XML Like syntax
-//JSX - transpiled to JS(React.createElement) - by Babel(JS compiler/transpiler) - that is inside Parcel
-const jsxHeading = (<h1 id="heading" className="head" tabIndex="1">React using JSX</h1>);
+//React Element
+const heading = (<h1 id="heading" className="head" tabIndex="1">React Element using JSX</h1>);
 
-console.log(jsxHeading);
+//React Components:- Returns JSX elements and name starts with capital
+//1. Class Bases Component - OLD - Based on JS classes
+
+
+//2. Functional Component - NEW - Based on JS functions
+const HeadingComponent = () => {
+return <div id="container">
+    <h1 className="heading">React Functional Component</h1>
+    {heading} 
+    <HeadingComponent2/>
+    </div>;
+    // above are the ways you render a element{} and a component</> inside another component aka component composition(only for the component part)
+};
+const HeadingComponent2 = () => <h1>React Functional Component2</h1>;
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxHeading); //replace everything inside the root  in  html
+root.render(heading); //this is how a react element is rendered
+root.render(<HeadingComponent />); //this is how a react component is rendered
 
