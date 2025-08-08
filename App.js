8371bcb026628939818e1374bfe71efdf522fb9f
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-08 23:52:55 +0530

    ep 4 food delievery app header

--- App.js
+++ App.js
@@ -1,34 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
  
-const elem = <span> React Element</span>
+/**
+ * Food ALix(food ordering) Website
+ * 
+ * Header
+ *  -Logo
+ *  -Nav Items
+ * Body
+ *  -Search Bar
+ *  -Restaurant Container
+ *    -Restaurant Card
+ * Footer
+ *  -Copyright
+ *  -Links
+ *  -Address
+ *  -Contact
+ */
 
-const heading = (<h1 id="heading" className="head" tabIndex="1">React Element using JSX {elem}</h1>);
-
-//React Components:- Returns JSX (or react element) and name starts with capital
-//1. Class Bases Component - OLD - Based on JS classes
-
-
-//2. Functional Component - NEW - Based on JS functions
-const HeadingComponent = () => {
-return <div id="container">
-    <h1 className="heading">React Functional Component</h1>
-    {heading} 
-    {100+200}
-    <HeadingComponent2/>
-    <HeadingComponent2></HeadingComponent2>
-    {HeadingComponent2()}
-    </div>;
-    // above are the ways you render a element or a variable or a piece of js code{} and a component<></> or </> inside another component aka component composition(only for the component part)
-
-    //another way of rendering the compont inside component is { ()} as at the end of the way any js code can be put inside curly and component is nothing but JS function
+//Header
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo">
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
 };
-const HeadingComponent2 = () => <h1>React Functional Component2</h1>;
 
 
+
+// The main div 
+const Applayout = () =>{
+    return (
+        <div className="app">
+           <Header/>
+        </div>
+        
+    );
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); //this is how a react element is rendered
-root.render(<HeadingComponent />); //this is how a react component is rendered
-
+root.render(<Applayout/>);
