94039c5cd00e1f028e235da5512174dfa95933ed
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-09 04:26:21 +0530

    ep4 some work on body and css

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import ReactDOM from "react-dom/client";
  *  -Search Bar
  *  -Restaurant Container
  *    -Restaurant Card
+ *      -Img
+ *      -Res name, star rating, cuisines, est delievery time
  * Footer
  *  -Copyright
  *  -Links
@@ -37,13 +39,55 @@ const Header = () => {
     );
 };
 
+//Styling for rest card
+const styleCard = {
+    backgroundColor: "#f0f0f0",
+    
+}
 
+//rest card for body
+const RestaurantCard = () =>{
+  return (
+    // or style = {{backgroundcolor:"gray"}} style expects object in jsx
+    <div className="res-card" style={styleCard}> 
+    <img className="res-logo" src="https://b.zmtcdn.com/data/pictures/chains/1/50691/ecfdc75d21bd7cd9d880f05c8382bc65.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"></img>
+      <h3>Rahamatuhlha</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>3.2 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+//Body
+const Body = () => {
+    return (
+          <div className="body">
+            <div className="search">
+               Search
+            </div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+          </div>
+    );
+};
 
 // The main div 
 const Applayout = () =>{
     return (
         <div className="app">
            <Header/>
+           <Body/>
         </div>
         
     );
