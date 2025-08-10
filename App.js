6ec28c38c965689822e8644788f3d28020a0eed4
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-11 04:06:07 +0530

    .join for cuisines array

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
- 
+
 /**
  * Food ALix(food ordering) Website
  * 
@@ -22,7 +22,7 @@ import ReactDOM from "react-dom/client";
 
 //Header
 const Header = () => {
-    return(
+    return (
         <div className="header">
             <div className="logo">
                 <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"></img>
@@ -42,56 +42,147 @@ const Header = () => {
 //Styling for rest card
 const styleCard = {
     backgroundColor: "#f0f0f0",
-    
+
 }
 
 //rest card for body
-const RestaurantCard = (props) =>{ //or we can destructure it like = ({resName, cuisine}) this will convert them in const and we can directly use these name in the {} instead of {prop.resName}
-    console.log(props);
-    const{resName, cuisine} = props;  // destructuring 
-  return (
-    <div className="res-card" style={styleCard}> 
-    <img className="res-logo" src="https://b.zmtcdn.com/data/pictures/chains/1/50691/ecfdc75d21bd7cd9d880f05c8382bc65.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"></img>
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>3.2 stars</h4>
-      <h4>38 minutes</h4>
-    </div>
-  );
+const RestaurantCard = (props) => { // in props its like {resData: resObj} 
+   const {resData} = props;  // here it means resData = resData inside of props therefore indireclty resData = resObj
+    return (
+        <div className="res-card" style={styleCard}>
+            <img className="res-logo" src="https://b.zmtcdn.com/data/pictures/chains/1/50691/ecfdc75d21bd7cd9d880f05c8382bc65.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"></img>
+            <h3>{resData.info.name}</h3>
+            <h4>{resData.info.cuisines.join(", ")}</h4> {/* here .join joins all the strings in array cuisines with , between */}
+            <h4>{resData.info.avgRating}</h4>
+            <h4>{resData.info.sla.slaString}</h4>
+        </div>
+    );
 };
+
+const resObj =
+{
+    "info": {
+        "id": "426776",
+        "name": "Theobroma",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/8/3/8086c92c-d573-4d83-b524-06530355f773_426776.JPG",
+        "locality": "7th Block",
+        "areaName": "Koramangala",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+            "Desserts",
+            "Bakery",
+            "Beverages"
+        ],
+        "avgRating": 4.6,
+        "parentId": "1040",
+        "avgRatingString": "4.6",
+        "totalRatingsString": "6.4K+",
+        "sla": {
+            "deliveryTime": 20,
+            "lastMileTravel": 1.3,
+            "serviceability": "SERVICEABLE",
+            "slaString": "15-20 mins",
+            "lastMileTravelString": "1.3 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2025-08-10 23:45:00",
+            "opened": true
+        },
+        "badges": {
+            "imageBadges": [
+                {
+                    "imageId": "Rxawards/_CATEGORY-Desserts.png",
+                    "description": "Delivery!"
+                },
+                {
+                    "imageId": "newg.png",
+                    "description": "Gourmet"
+                }
+            ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+            "entityBadges": {
+                "imageBased": {
+                    "badgeObject": [
+                        {
+                            "attributes": {
+                                "description": "Delivery!",
+                                "imageId": "Rxawards/_CATEGORY-Desserts.png"
+                            }
+                        },
+                        {
+                            "attributes": {
+                                "description": "Gourmet",
+                                "imageId": "newg.png"
+                            }
+                        }
+                    ]
+                },
+                "textBased": {},
+                "textExtendedBadges": {}
+            }
+        },
+        "aggregatedDiscountInfoV3": {
+            "header": "ITEMS",
+            "subHeader": "AT ₹290"
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+                "lottie": {},
+                "video": {}
+            }
+        },
+        "reviewsSummary": {},
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {},
+        "externalRatings": {
+            "aggregatedRating": {
+                "rating": "--"
+            }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+        "context": "seo-data-f09b1f11-2b3f-4c1a-b573-e7abd25c97fb"
+    },
+    "cta": {
+        "link": "https://www.swiggy.com/city/bangalore/theobroma-7th-block-koramangala-rest426776",
+        "type": "WEBLINK"
+    }
+};
+
 
 //Body
 const Body = () => {
     return (
-          <div className="body">
+        <div className="body">
             <div className="search">
-               Search
+                Search
             </div>
             <div className="res-container">
-                <RestaurantCard 
-                 resName="Rahmatulha"
-                 cuisine = "Biryani, North Indian, Asian"
-                />
                 <RestaurantCard
-                resName="KFC"
-                cuisine = "Fast Food, Burger"
-                />  
+                    resData = {resObj}
+                />
             </div>
-          </div>
+        </div>
     );
 };
 
 // The main div 
-const Applayout = () =>{
+const Applayout = () => {
     return (
         <div className="app">
-           <Header/>
-           <Body/>
+            <Header />
+            <Body />
         </div>
-        
+
     );
 };
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Applayout/>);
+root.render(<Applayout />);
