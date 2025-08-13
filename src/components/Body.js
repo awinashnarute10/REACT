92caabf0d55efd2e559979c8f0b28c0fe157c9d2
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-14 04:45:05 +0530

    ep 5 problem of ui not changing with change in data/ value that will be solved with hooks in next commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,8 +5,16 @@ import resList from "../utils/mockData";
 const Body = () => {
     return (
         <div className="body">
-            <div className="search">
-                Search
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    console.log("Button CLicked");
+                    // filter logic so that when this btn is clicked is only shows restaurant with rating>4.5
+                    resList = resList.filter( // 2 problems here resList is not defined here but even if it was defined in this file there wouldnt be any change in ui because after this only the value of relist will change but the DOM wont re render so ui wont change 
+                        (restaurant)=>restaurant.info.avgRating>4.6
+                    );
+                    console.log(resList);
+                }}>Top Rated Restaurants</button>
+
             </div>
             <div className="res-container">
                { resList.map((restaurant)=>(
