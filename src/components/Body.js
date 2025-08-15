f7014fb08ca3c9dfbb77682275da00face4650e5
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-15 20:41:33 +0530

    minor changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,10 +9,11 @@ const Body = () => {
                 <button className="filter-btn" onClick={() => {
                     console.log("Button CLicked");
                     // filter logic so that when this btn is clicked is only shows restaurant with rating>4.5
-                    resList = resList.filter( // 2 problems here resList is not defined here but even if it was defined in this file there wouldnt be any change in ui because after this only the value of relist will change but the DOM wont re render so ui wont change 
+                    resList = resList.filter( // 2 problems here resList is not defined here (because resList is const in mockData.js) but even if it was defined in this file there wouldnt be any change in ui because after this only the value of relist will change but the DOM wont re render so ui wont change 
                         (restaurant)=>restaurant.info.avgRating>4.6
                     );
                     console.log(resList);
+                    render();
                 }}>Top Rated Restaurants</button>
 
             </div>
