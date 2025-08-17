51eec467945828ff081f8a427e9aa82e751e3606
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-17 23:53:08 +0530

    ep 6 used useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,40 @@
-import RestaurantCard from "./RestaurantCard"; // default import 
+import RestaurantCard from "./RestaurantCard"; 
 import resList from "../utils/mockData";
-import { use, useState } from "react";
+import {useEffect, useState } from "react";
 
 //Body
 const Body = () => {
-    // Destructring the useState into statevariable and statesetter 
-    const[resListState, setresListState] = useState(resList);
+    // at the beggining the resliststate is empty 
+    const[resListState, setresListState] = useState([]);
+
+    // useEffect hook used for rendering once after the original ui is rendered to fetch data and render again
+   useEffect(()=>{
+    fetchData();
+   }, [])
+
+    const fetchData = async () => {
+        // fetch from swiggy api
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.8396589&lng=74.61125129999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        // convert data into json
+        const json = await data.json();
+
+
+        //  set the resListState to array from the api and after this is set the ui will render again
+        setresListState(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    
+    // first render itll show Loading 
+    if(resListState.length===0){
+        return(
+            <h1>Loading......</h1>
+        )
+    }
+
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
                     console.log("Button CLicked");
-                    // filtering the resListState and changing the state variable using statesetter therefore this compnent can be re rendered 
                     setresListState(resListState.filter((restaurant)=>restaurant.info.avgRating>4.6))
                     
                 }}>Top Rated Restaurants</button>
