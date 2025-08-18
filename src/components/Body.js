15dba840827395adf0c9c7d69ba14db60900e3bc
Awinash Narute <awinashnarute4228@gmail.com>
2025-08-18 16:10:41 +0530

    ep 6 shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,32 +1,29 @@
-import RestaurantCard from "./RestaurantCard"; 
+import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 //Body
 const Body = () => {
-    // at the beggining the resliststate is empty 
-    const[resListState, setresListState] = useState([]);
+    const [resListState, setresListState] = useState([]);
 
-    // useEffect hook used for rendering once after the original ui is rendered to fetch data and render again
-   useEffect(()=>{
-    fetchData();
-   }, [])
+    useEffect(() => {
+        fetchData();
+    }, [])
 
     const fetchData = async () => {
-        // fetch from swiggy api
+
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.8396589&lng=74.61125129999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        // convert data into json
+
         const json = await data.json();
 
-
-        //  set the resListState to array from the api and after this is set the ui will render again
         setresListState(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
-    
+
     // first render itll show Loading 
-    if(resListState.length===0){
-        return(
-            <h1>Loading......</h1>
+    if (resListState.length === 0) {
+        return (
+            <Shimmer/>   // shimmer ui till data is not fetched
         )
     }
 
@@ -35,14 +32,14 @@ const Body = () => {
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
                     console.log("Button CLicked");
-                    setresListState(resListState.filter((restaurant)=>restaurant.info.avgRating>4.6))
-                    
+                    setresListState(resListState.filter((restaurant) => restaurant.info.avgRating > 4.6))
+
                 }}>Top Rated Restaurants</button>
 
             </div>
             <div className="res-container">
-               { resListState.map((restaurant)=>(
-                    <RestaurantCard key={restaurant.info.id} resData = {restaurant}/>  
+                {resListState.map((restaurant) => (
+                    <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                 ))}
             </div>
         </div>
