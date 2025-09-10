64c5f518eaf0283bd74303a2417a3fe41597b576
Awinash Narute <awinashnarute4228@gmail.com>
2025-09-10 12:03:51 +0530

    ep 8 lifecycle of class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { Component } from "react";  
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
+// changed the about from functional component to class based component to understand life cycle of class based component
+
+class About extends Component{  //  as we imported Component from react we dont have to write React.Component 
+  constructor(props){
+    super(props);
+
+    console.log("Parent constructor")
+  }
+
+  componentDidMount(){
+    console.log("Parent did mount")
+  }
+
+  render(){
+
+    console.log("Parent rendered")
+
     return(
           <div>
             <h1>About Us Page </h1>
-            <User name="Awinash Function" location="Sangli" contact = "N/A"/>
-            <UserClass name="Awinash class" location="Sangli" contact = "N/A"/>
+            
+            <UserClass name="First class" location="Sangli" contact = "N/A"/>
+            <UserClass name="Second class" location="Vasco" contact = "N/A"/>
+
           </div>
     );
+  }
 }
+    
+
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,15 +5,27 @@ class UserClass extends React.Component {
 
     constructor(props){ // props are passes to constructor in which its initalized by super that initializes it in React.component class
         super(props);
+
         this.state = {   // initalizing and declaring state variables in class component 
           count: 0
         };
+
+        console.log(this.props.name + "Consturctor called")
+    }
+
+
+    componentDidMount(){
+        console.log(this.props.name+ "did mount called")    // to understand class based component life cycle 
     }
 
     render() {
+         
+        console.log(this.props.name+ "render called")
+
        const {name, location, contact} = this.props;
 
        const {count} = this.state; // destructuring state variable
+
         return(
             <div className="user-card">
            <h2>Name:{name}</h2>
@@ -26,8 +38,10 @@ class UserClass extends React.Component {
             });
            }}
            >Count Increase</button>
+
            <h3>Location:{location}</h3>
            <h4>Contact: {contact}</h4>
+
         </div>
         );
     };
